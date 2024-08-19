4445f7fca56ede26aad3a2c76a792486e31bce99
millmai <6529011016@cdti.ac.th>
2024-08-19 16:34:30 +0700

    update

---
+++ myweb/members/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def members(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
+
+def profile(request):
+    template = loader.get_template('profile.html')
+    return HttpResponse(template.render())
