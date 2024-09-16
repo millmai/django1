86b457f9dc7d0288d76a1a79fd58c5395fe63380
millmai <6529011016@cdti.ac.th>
2024-09-16 16:07:09 +0700

    update

--- project1/about/views.py
+++ project1/about/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def about(request):
+    mymembers = Member.objects.all().values()
     template = loader.get_template('about.html')
-    return HttpResponse(template.render())
+    context = {
+        'mymembers': mymembers,
+    }
+
+    return HttpResponse(template.render(context, request))
+
